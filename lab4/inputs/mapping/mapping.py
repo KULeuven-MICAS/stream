025cbfb97272b4ac4f11dab333ca3c5831369e85
asyms <arne.symons@kuleuven.be>
2023-04-22 18:35:59 +0200

    lab4 updates

--- lab4/inputs/mapping/mapping.py
+++ lab4/inputs/mapping/mapping.py
@@ -2,4 +2,25 @@ mapping = {
     "Conv": {
         "core_allocation": [0, 1, 2, 3],
     },
+    "Gemm": {
+        "core_allocation": [0, 1, 2, 3],
+    },
+    "Pool": {
+        "core_allocation": [4],
+    },
+    "MaxPool": {
+        "core_allocation": [4],
+    },
+    "AveragePool": {
+        "core_allocation": [4],
+    },
+    "GlobalAveragePool": {
+        "core_allocation": [4],
+    },
+    "Add": {
+        "core_allocation": [5],
+    },
+    "default": {
+        "core_allocation": [0, 1, 2, 3],
+    },
 }
