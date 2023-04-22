42af798b4667463db0bf5a217d22f68118c4d9ea
asyms <arne.symons@kuleuven.be>
2023-04-22 16:23:58 +0200

    update lab4 with new experiments

--- lab4/inputs/hardware/heterogeneous_quadcore.py
+++ lab4/inputs/hardware/heterogeneous_quadcore.py
@@ -12,18 +12,12 @@ from stream.inputs.examples.hardware.nocs.mesh_2d import get_2d_mesh
 from stream.classes.hardware.architecture.accelerator import Accelerator
 
 
-# Because it's a quadcore system, we need to rescale the memory sizes
-core_def.size_l2_weights = core_def.size_l2_weights / 4
-core_def.size_l2_activation = core_def.size_l2_activation / 4
-core_def.size_l1_weights = core_def.size_l1_weights / 4
-core_def.size_l1_activation = core_def.size_l1_activation / 4
-
 # Get all the computational cores, assigning an id
 cores = [
-    get_ox_fx_fy_core(id=0, quad_core=True),
-    get_ox_k_core(id=1, quad_core=True),
-    get_c_k_core(id=2, quad_core=True),
-    get_c_k_core(id=3, quad_core=True),
+    get_ox_fx_fy_core(id=0),
+    get_ox_k_core(id=1),
+    get_c_k_core(id=2),
+    get_c_k_core(id=3),
 ]
 
 pooling_core = get_pooling_core(id=4)
@@ -31,7 +25,9 @@ simd_core = get_simd_core(id=5)
 offchip_core_id = 6
 offchip_core = get_offchip_core(id=offchip_core_id)
 
-cores_graph = get_2d_mesh(cores, 2, 2, core_def.inter_core_bandwidth, 0, pooling_core, simd_core, offchip_core)
+cores_graph = get_2d_mesh(
+    cores, 2, 2, core_def.inter_core_bandwidth, 0, pooling_core, simd_core, offchip_core
+)
 
 accelerator = Accelerator(
     "heterogeneous-quadcore",
