e0e4ba1ff404f66805ae1cf8d89168e6427d2af5
asyms <arne.symons@kuleuven.be>
2023-07-08 10:18:48 +0200

    add adaptive split model save location based on experiment id; add double-buffered friendly layer splitting flag

--- main_stream_exploration_lbl_splitting.py
+++ main_stream_exploration_lbl_splitting.py
@@ -5,13 +5,34 @@ from stream.visualization.memory_usage import plot_memory_usage
 import argparse
 import pickle
 import re
+import os
 
 
 parser = argparse.ArgumentParser(description="Setup zigzag-v2 inputs")
-parser.add_argument('--workload_path', metavar='path', required=True, help='module path to workload, e.g. inputs.examples.workloads.resnet18')
-parser.add_argument('--mapping_path', metavar='path', required=True, help='path to mapping file, e.g., inputs.examples.mapping.tpu_like')
-parser.add_argument('--accelerator', metavar='path', required=True, help='module path to the accelerator, e.g. inputs.examples.hardware.TPU_like')
-parser.add_argument('--headname', metavar='path', required=True, help='module path to the accelerator, e.g. inputs.examples.hardware.TPU_like')
+parser.add_argument(
+    "--workload_path",
+    metavar="path",
+    required=True,
+    help="module path to workload, e.g. inputs.examples.workloads.resnet18",
+)
+parser.add_argument(
+    "--mapping_path",
+    metavar="path",
+    required=True,
+    help="path to mapping file, e.g., inputs.examples.mapping.tpu_like",
+)
+parser.add_argument(
+    "--accelerator",
+    metavar="path",
+    required=True,
+    help="module path to the accelerator, e.g. inputs.examples.hardware.TPU_like",
+)
+parser.add_argument(
+    "--headname",
+    metavar="path",
+    required=True,
+    help="module path to the accelerator, e.g. inputs.examples.hardware.TPU_like",
+)
 args = parser.parse_args()
 
 
@@ -52,12 +73,14 @@ plot_data_transfer = True
 nb_ga_individuals = 96  # number of individuals in each genetic algorithm generation
 nb_ga_generations = 16  # number of genetic algorithm generations
 
-root_path = f"/esat/prometheus1/users/lmei/Stream_2023_TC_exploration_results4_latency"
+root_path = f"./testing_exploration_issues"
 intra_result_path = f"{root_path}/intra_result/"
 inter_result_path = f"{root_path}/inter_result/"
 plot_path = f"{root_path}/plot/"
 filename = f"{args.headname}-lbl-{node_hw_cost_pkl_name}"
 node_hw_performances_path = f"{intra_result_path}{filename}.pickle"
+split_onnx_model_path = f"{root_path}/split_models/model_split-{experiment_id}.onnx"
+split_W_double_buffered = True
 #################################
 
 
@@ -88,6 +111,8 @@ mainstage = MainStage(
     operands_to_prefetch=[],
     scheduler_candidate_selection="latency",
     visualize_node_hw_performances_path=f"{intra_result_path}{filename}.png",
+    split_onnx_model_path=split_onnx_model_path,
+    split_W_double_buffered=split_W_double_buffered,
 )
 
 # Launch the MainStage
@@ -95,7 +120,12 @@ scme, _ = mainstage.run()
 scme = scme[0]
 
 # Save result
-with open(f"{inter_result_path}{filename}.pickle", "wb") as f:
+result_filename = f"{inter_result_path}{filename}.pickle"
+# Create subfolders for result if they don't exist
+dir_name = os.path.dirname(os.path.abspath(result_filename))
+if not os.path.isdir(dir_name):
+    os.makedirs(dir_name)
+with open(result_filename, "wb") as f:
     pickle.dump(scme, f, -1)
 
 # Ploting Results
@@ -119,8 +149,11 @@ plot_memory_usage(scme, section_start_percent, percent_shown, fig_path=memory_fi
 
 # Print Results
 import logging
+
 logger = logging.getLogger(__name__)
 latency = scme.latency
 energy = scme.energy
 edp = latency * energy
-logger.info(f"Experiment {args.headname} Results: Latency = {int(latency):.4e} Cycles   Energy = {energy:.4e} pJ   EDP = {edp:.4e}")
+logger.info(
+    f"Experiment {args.headname} Results: Latency = {int(latency):.4e} Cycles   Energy = {energy:.4e} pJ   EDP = {edp:.4e}"
+)
