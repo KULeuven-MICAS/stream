7a69914079e51e3370621125180fd135857a1e00
Gagandeep Singh <gagandeep.singh@amd.com>
2023-07-20 11:57:34 -0600

    bottleneck mapping update

--- stream/inputs/aie/testing_mapping_bottleneck.py
+++ stream/inputs/aie/testing_mapping_bottleneck.py
@@ -2,33 +2,23 @@ mapping = {
     "Conv1": {
         "core_allocation": [0],
     },
-    # "Conv2": {
-    #     "core_allocation": [1],
-    # },
+    "Conv2": {
+        "core_allocation": [1],
+    },
+    "Conv22": {
+        "core_allocation": [2],
+    },
+
     "Conv3": {
         "core_allocation": [3],
     },
-
     "Add": {
         "core_allocation": [0],
     },
-    # "Gemm": {
-    #     "core_allocation": [0, 1, 2, 3],
-    # },
-    # "Pool": {
-    #     "core_allocation": [4],
-    # },
-    # "MaxPool": {
-    #     "core_allocation": [4],
-    # },
-    # "AveragePool": {
-    #     "core_allocation": [4],
-    # },
-    # "GlobalAveragePool": {
-    #     "core_allocation": [4],
-    # },
-    
     "default": {
-        "core_allocation": [ 1, 2],
+        "core_allocation": [1, 2,3],
     },
+    # "default": {
+    #     "core_allocation": [0,1,2,3],
+    # },
 }
