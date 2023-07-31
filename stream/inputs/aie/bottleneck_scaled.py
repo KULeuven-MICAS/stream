babe51b17bf9261edb1bcbcf987b577cd0e3fbac
Gagandeep Singh <gagandeep.singh@amd.com>
2023-07-31 10:21:46 -0600

    aie with latest changes

--- stream/inputs/aie/bottleneck_scaled.py
+++ stream/inputs/aie/bottleneck_scaled.py
@@ -1,6 +1,6 @@
 workload = {
     0: {
-        "operator_type": "Conv",
+        "operator_type": "Conv1",
         "equation": "O[b][k][oy][ox]+=W[k][c][fy][fx]*I[b][c][iy][ix]",
         "dimension_relations": ["ix=1*ox+1*fx", "iy=1*oy+1*fy"],
         "loop_dim_size": {
@@ -19,7 +19,7 @@ workload = {
         "padding": {"IY": (0, 0), "IX": (0, 0)},
     },
     1: {
-        "operator_type": "Conv",
+        "operator_type": "Conv2",
         "equation": "O[b][k][oy][ox]+=W[k][c][fy][fx]*I[b][c][iy][ix]",
         "dimension_relations": ["ix=1*ox+1*fx", "iy=1*oy+1*fy"],
         "loop_dim_size": {
@@ -38,7 +38,7 @@ workload = {
         "padding": {"IY": (0, 0), "IX": (0, 0)},
     },
     11: {
-        "operator_type": "Conv",
+        "operator_type": "Conv22",
         "equation": "O[b][k][oy][ox]+=W[k][c][fy][fx]*I[b][c][iy][ix]",
         "dimension_relations": ["ix=1*ox+1*fx", "iy=1*oy+1*fy"],
         "loop_dim_size": {
@@ -57,7 +57,7 @@ workload = {
         "padding": {"IY": (0, 0), "IX": (0, 0)},
     },
     2: {
-        "operator_type": "Conv",
+        "operator_type": "Conv3",
         "equation": "O[b][k][oy][ox]+=W[k][c][fy][fx]*I[b][c][iy][ix]",
         "dimension_relations": ["ix=1*ox+1*fx", "iy=1*oy+1*fy"],
         "loop_dim_size": {
@@ -86,6 +86,94 @@ workload = {
     #     'operand_source_dimension_mapping': {'X': {'OX': 'OX', 'OY': 'OY', 'G': 'K'}, 'Y': {'OX': 'OX', 'OY': 'OY', 'G': 'K'}},
     #     'memory_operand_links': {'O': 'O', 'X': 'I2', 'Y': 'I1'}
     # }
+############################
+    3: {
+        "operator_type": "Conv1",
+        "equation": "O[b][k][oy][ox]+=W[k][c][fy][fx]*I[b][c][iy][ix]",
+        "dimension_relations": ["ix=1*ox+1*fx", "iy=1*oy+1*fy"],
+        "loop_dim_size": {
+            "B": 1,
+            "K": 64,
+            "C": 256,
+            "OY": 112,
+            "OX": 112,
+            "FY": 1,
+            "FX": 1,
+        },
+        "operand_precision": {"O": 8, "O_final": 8, "W": 8, "I": 8},
+        "operand_source": {"W": [], "I": []},
+        "constant_operands": ["W", "I"],
+        "memory_operand_links": {"O": "O", "W": "I2", "I": "I1"},
+        "padding": {"IY": (0, 0), "IX": (0, 0)},
+    },
+    4: {
+        "operator_type": "Conv2",
+        "equation": "O[b][k][oy][ox]+=W[k][c][fy][fx]*I[b][c][iy][ix]",
+        "dimension_relations": ["ix=1*ox+1*fx", "iy=1*oy+1*fy"],
+        "loop_dim_size": {
+            "B": 1,
+            "K": 32,
+            "C": 64,
+            "OY": 112,
+            "OX": 112,
+            "FY": 3,
+            "FX": 3,
+        },
+        "operand_precision": {"O": 8, "O_final": 8, "W": 8, "I": 8},
+        "operand_source": {"W": [], "I": [0]},
+        "constant_operands": ["W"],
+        "memory_operand_links": {"O": "O", "W": "I2", "I": "I1"},
+        "padding": {"IY": (0, 0), "IX": (0, 0)},
+    },
+    44: {
+        "operator_type": "Conv22",
+        "equation": "O[b][k][oy][ox]+=W[k][c][fy][fx]*I[b][c][iy][ix]",
+        "dimension_relations": ["ix=1*ox+1*fx", "iy=1*oy+1*fy"],
+        "loop_dim_size": {
+            "B": 1,
+            "K": 32,
+            "C": 64,
+            "OY": 112,
+            "OX": 112,
+            "FY": 3,
+            "FX": 3,
+        },
+        "operand_precision": {"O": 8, "O_final": 8, "W": 8, "I": 8},
+        "operand_source": {"W": [], "I": [0]},
+        "constant_operands": ["W"],
+        "memory_operand_links": {"O": "O", "W": "I2", "I": "I1"},
+        "padding": {"IY": (0, 0), "IX": (0, 0)},
+    },
+    5: {
+        "operator_type": "Conv3",
+        "equation": "O[b][k][oy][ox]+=W[k][c][fy][fx]*I[b][c][iy][ix]",
+        "dimension_relations": ["ix=1*ox+1*fx", "iy=1*oy+1*fy"],
+        "loop_dim_size": {
+            "B": 1,
+            "K": 256,
+            "C": 64,
+            "OY": 112,
+            "OX": 112,
+            "FY": 1,
+            "FX": 1,
+        },
+        "operand_precision": {"O": 8, "O_final": 8, "W": 8, "I": 8},
+        "operand_source": {"W": [], "I": [1]},
+        "constant_operands": ["W"],
+        "memory_operand_links": {"O": "O", "W": "I2", "I": "I1"},
+        "padding": {"IY": (0, 0), "IX": (0, 0)},
+    },
+    6: {  # Addition of layer 1 (residual path) and layer 3 (main path)
+        'operator_type': 'Add',
+        'equation': 'O[b][g][oy][ox]=X[b][g][oy][ox]+Y[b][g][oy][ox]',
+        'dimension_relations': [],
+        'loop_dim_size': {'B': 1, 'G': 256, 'OY': 112, 'OX': 112},
+        'operand_precision': {'O': 8, 'O_final': 8, 'X': 8, 'Y': 8},
+        'operand_source': {'X': [5], 'Y': [2]},
+        'constant_operands': [],
+        'operand_source_dimension_mapping': {'X': {'OX': 'OX', 'OY': 'OY', 'G': 'K'}, 'Y': {'OX': 'OX', 'OY': 'OY', 'G': 'K'}},
+        'memory_operand_links': {'O': 'O', 'X': 'I2', 'Y': 'I1'}
+    }
     # 3: {
     #     "operator_type": "Add",
     #     "equation": "O[b][k][oy][ox]+=W[k][c][fy][fx]*I[b][c][iy][ix]",
--- stream/inputs/aie/hardware/aie_col.py
+++ stream/inputs/aie/hardware/aie_col.py
@@ -13,14 +13,14 @@ from stream.inputs.aie.hardware.aie_core2 import (
 from stream.inputs.aie.hardware.shim_dma_core import (
     get_shim_dma_core as shim_core
 )
-
+from stream.inputs.examples.hardware.cores.pooling import get_core as get_pooling_core
 from stream.inputs.examples.hardware.nocs.mesh_2d import get_2d_mesh
 from stream.classes.hardware.architecture.accelerator import Accelerator
 
 cores = [aie_core1(0),aie_core2(1),aie_core2(2),aie_core4(3)]  # 4 cores
-
+pooling_core = get_pooling_core(id=4)
 # mem_tile_core = mem_tile(id=4)
-offchip_core_id = 4
+offchip_core_id = 5
 offchip_core = shim_core(id=offchip_core_id) # basically DRAM
 
 # Comment out the offchip_bandwidth because we can get this attribute from the offchip_core (if it is defined), thus no need to manually define it
@@ -29,7 +29,7 @@ cores_graph = get_2d_mesh(
     nb_rows=1,
     nb_cols=4,
     bandwidth=64*8,
-    # pooling_core=mem_tile_core,
+    pooling_core=pooling_core,
     unit_energy_cost=0,
     # mem_tile_core=mem_tile_core,
     offchip_core=offchip_core,
