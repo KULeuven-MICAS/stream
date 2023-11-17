3f307182a5e695187e75e5ff010698c64257a0c7
Gagandeep Singh <gagandeep.singh@amd.com>
2023-11-17 09:53:45 -0700

    matmul initial example

---
+++ stream/inputs/aie/testing_mapping_matmul.py
@@ -0,0 +1,11 @@
+mapping = {
+    "default": {
+        "core_allocation": [0,1],
+    },
+    "Conv": {
+        "core_allocation": [0],
+    },
+    "Gemm": {
+        "core_allocation": [0],
+    }
+}
