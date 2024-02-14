7af75f1a2cd624cf75cf196882d69fde9019e4a6
Arne Symons <34397549+asyms@users.noreply.github.com>
2024-02-14 10:56:43 +0000

    add example that parallelizes layer across cores

---
+++ lab4/inputs/mapping/mapping_fixed_parallel.py
@@ -0,0 +1,17 @@
+mapping = {
+    "layer_on_core0": {
+        "core_allocation": (0, 1, 2, 3),
+    },
+    "layer_on_core1": {
+        "core_allocation": (0, 1, 2, 3),
+    },
+    "layer_on_core2": {
+        "core_allocation": (0, 1, 2, 3),
+    },
+    "layer_on_core3": {
+        "core_allocation": (0, 1, 2, 3),
+    },
+    "default": {
+        "core_allocation": [0],
+    },
+}
