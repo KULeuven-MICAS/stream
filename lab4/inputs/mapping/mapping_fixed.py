aec676d23e9068ef45275a5b89a7acd974aedb64
Arne Symons <34397549+asyms@users.noreply.github.com>
2024-02-15 11:02:21 +0000

    update fixed mapping to use tuples

--- lab4/inputs/mapping/mapping_fixed.py
+++ lab4/inputs/mapping/mapping_fixed.py
@@ -1,14 +1,14 @@
 mapping = {
     "Layer 0": {
-        "core_allocation": [0],
+        "core_allocation": (0,),
     },
     "Layer 1": {
-        "core_allocation": [4],
+        "core_allocation": (4,),
     },
     "Layer 2": {
-        "core_allocation": [1],
+        "core_allocation": (1,),
     },
     "Layer 3": {
-        "core_allocation": [2],
+        "core_allocation": (2,),
     },
 }
