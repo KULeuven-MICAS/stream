fea72c0933a17a03986284ffa7bbf35eeb727f44
Arne Symons <arnesymons@hotmail.com>
2024-05-21 20:13:45 +0200

    rename lab4 to lab1 and add new ui inputs

--- demo.sh
+++ demo.sh
@@ -1,6 +1,1 @@
-python lab4/main_layer_sequential_fixed.py;
-python lab4/main_layer_sequential.py;
-python lab4/main_layer_parallel_fixed.py;
-python lab4/main_layer_parallel.py;
-python lab4/main_layer_fused_fixed.py;
-python lab4/main_layer_fused.py;
+python lab1/main.py;
--- lab1/main.py
+++ lab1/main.py
@@ -1,19 +1,19 @@
-import sys
 import os
+import sys
+import re
+import logging as _logging
 
-sys.path.insert(0, os.getcwd())
-from zigzag.classes.stages import *
-from stream.classes.stages import *
+sys.path.insert(0, os.getcwd())  # Insert main folder in path
+
+from stream.visualization.memory_usage import plot_memory_usage
 from stream.visualization.schedule import (
-    plot_timeline_brokenaxes,
     visualize_timeline_plotly,
 )
-from stream.visualization.memory_usage import plot_memory_usage
-import re
-import pickle
+from stream.classes.stages import *
+from zigzag.stages.MainStage import MainStage
+
 
 ############################## Initialize the logger ##############################
-import logging as _logging
 
 _logging_level = _logging.INFO
 _logging_format = "%(asctime)s - %(levelname)s - %(message)s"
@@ -21,9 +21,11 @@ _logging.basicConfig(level=_logging_level, format=_logging_format)
 ####################################################################################
 
 ############################## Provide inputs ######################################
-accelerator = "lab4.inputs.hardware.heterogeneous_quadcore"
-workload_path = "stream/inputs/examples/workload/resnet18.onnx"
-mapping_path = "lab4.inputs.mapping.mapping"
+workload_path = "lab1/inputs/workload/workload.yaml"
+accelerator = "lab1/inputs/hardware/hda_bus.yaml"
+mapping_path = "lab1/inputs/mapping/mapping.yaml"
+timeline_fig_path_plotly = f"lab1/outputs/timeline.html"
+memory_fig_path = f"lab1/outputs/memory_usage.png"
 ####################################################################################
 
 ############################## Define variables for run ############################
@@ -33,26 +35,26 @@ hw_name = accelerator.split(".")[-1]
 wl_name = re.split(r"/|\.", workload_path)[-1]
 if wl_name == "onnx":
     wl_name = re.split(r"/|\.", workload_path)[-2]
-hint_loops_str_list = []
-for dim, size in hint_loops:
-    hint_loops_str_list.extend([str(dim).lower(), str(size)])
-hint_loops_str = "_".join(hint_loops_str_list)
-experiment_id = f"{hw_name}-{wl_name}-hintloop_{hint_loops_str}"
+experiment_id = "lab1"
 node_hw_cost_pkl_name = f"saved_cn_hw_cost-{experiment_id}"
 plot_file_name = f"-{experiment_id}-"
 plot_full_schedule = True
 plot_data_transfer = True
-nb_ga_individuals = 16  # number of individuals in each genetic algorithm generation
-nb_ga_generations = 16  # number of genetic algorithm generations
-node_hw_performances_path = f"lab4/outputs/{node_hw_cost_pkl_name}.pickle"
+node_hw_performances_path = f"lab1/outputs/{node_hw_cost_pkl_name}.pickle"
+visualize_node_hw_performances_path = (
+    f"lab1/outputs/{node_hw_cost_pkl_name}_visualization.png"
+)
+nb_ga_generations = 0
+nb_ga_individuals = 0
 ####################################################################################
 
 
 mainstage = MainStage(
     [  # Initializes the MainStage as entry point
         AcceleratorParserStage,  # Parses the accelerator
-        StreamONNXModelParserStage,  # Parses the ONNX Model into the workload
-        # UserDefinedModelParserStage,  # Parses the user-defined Model into the workload
+        # StreamONNXModelParserStage,  # Parses the ONNX Model into the workload
+        UserDefinedModelParserStage,  # Parses the user-defined Model into the workload
+        # ProfileWorkloadStage,
         GenerateCNWorkloadHybridStage,
         IntraCoreMappingStage,
         InterCoreMappingStage,
@@ -61,34 +63,21 @@ mainstage = MainStage(
     workload_path=workload_path,  # required by ModelParserStage
     mapping_path=mapping_path,  # required by ModelParserStage
     loma_lpf_limit=6,  # required by LomaStage
-    nb_ga_individuals=nb_ga_individuals,
-    nb_ga_generations=nb_ga_generations,
     node_hw_performances_path=node_hw_performances_path,  # saved node_hw_performances to skip re-computation
     plot_hof=True,  # Save schedule and memory usage plot of each individual in the Genetic Algorithm hall of fame
     plot_file_name=plot_file_name,
-    plot_full_schedule=plot_full_schedule,
-    plot_data_transfer=plot_data_transfer,
     cn_define_mode=CN_define_mode,
     hint_loops=hint_loops,
     scheduler_candidate_selection="latency",
-    visualize_node_hw_performances=True,
+    visualize_node_hw_performances_path=visualize_node_hw_performances_path,
     operands_to_prefetch=[],
-
+    nb_ga_generations=nb_ga_generations,
+    nb_ga_individuals=nb_ga_individuals,
 )
 
-# Unpickle the best SCME if we already ran this experiment
-pickle_path = f"lab4/outputs/{experiment_id}_best_scme.pickle"
-if os.path.exists(pickle_path):
-    with open(pickle_path, "rb") as fp:
-        scme = pickle.load(fp)
-else:
-    # Launch the MainStage
-    scme, _ = mainstage.run()
-    scme = scme[0]
-
-# Pickle the best SCME for later re-plotting
-with open(pickle_path, "wb") as fp:
-    pickle.dump(scme, fp)
+# Launch the MainStage
+scme, _ = mainstage.run()
+scme = scme[0]
 
 # Ploting Results
 plot_full_schedule = True
@@ -96,8 +85,7 @@ draw_dependencies = True
 plot_data_transfer = True
 section_start_percent = (0,)
 percent_shown = (100,)
-fig_path = f"lab4/outputs/timeline-{experiment_id}.png"
-timeline_fig_path_plotly = f"lab4/outputs/timeline-{experiment_id}.html"
+
 
 # Plotting results using Plotly
 visualize_timeline_plotly(
@@ -107,12 +95,4 @@ visualize_timeline_plotly(
     fig_path=timeline_fig_path_plotly,
 )
 
-# Plotting results using brokenaxes
-plot_timeline_brokenaxes(
-    scme,
-    draw_dependencies,
-    section_start_percent,
-    percent_shown,
-    plot_data_transfer,
-    fig_path=fig_path,
-)
+plot_memory_usage(scme, section_start_percent, percent_shown, fig_path=memory_fig_path)
