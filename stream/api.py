a2fd59c0651bd2c7bae21246ecea16e8d2fb32da
Arne Symons <arnesymons@hotmail.com>
2024-09-23 09:10:21 +0200

    update genetic algorithm api call

--- stream/api.py
+++ stream/api.py
@@ -1,55 +1,67 @@
+import os
 from zigzag.stages.main import MainStage
+from zigzag.utils import pickle_load
 
 from stream.stages.allocation.genetic_algorithm_allocation import GeneticAlgorithmAllocationStage
 from stream.stages.estimation.zigzag_core_mapping_estimation import ZigZagCoreMappingEstimationStage
+from stream.stages.generation.hint_loops_generation import HintLoopsGenerationStage
 from stream.stages.generation.hint_loops_partitioned_workload_generation import (
     HintLoopsPartitionedWorkloadGenerationStage,
 )
+from stream.stages.generation.layer_stacks_generation import LayerStacksGenerationStage
+from stream.stages.generation.scheduling_order_generation import SchedulingOrderGenerationStage
 from stream.stages.parsing.accelerator_parser import AcceleratorParserStage
-from stream.stages.parsing.onnx_model_parser import UserDefinedModelParserStage
+from stream.stages.parsing.onnx_model_parser import ONNXModelParserStage as StreamONNXModelParserStage
+from stream.stages.set_fixed_allocation_performance import SetFixedAllocationPerformanceStage
 
+import logging as _logging
 
-def get_hardware_performance_stream(hardware, workload, mapping, CN_define_mode, hint_loops, node_hw_cost_pkl_name):
-    import logging as _logging
+def optimize_allocation_ga(hardware, workload, mapping, mode, experiment_id, output_path):
 
     _logging_level = _logging.INFO
     # _logging_format = '%(asctime)s - %(name)s.%(funcName)s +%(lineno)s - %(levelname)s - %(message)s'
     _logging_format = "%(asctime)s - %(funcName)s +%(lineno)s - %(levelname)s - %(message)s"
     _logging.basicConfig(level=_logging_level, format=_logging_format)
 
-    mainstage = MainStage(
-        [  # Initializes the MainStage as entry point
-            AcceleratorParserStage,  # Parses the accelerator
-            # StreamONNXModelParserStage,  # Parses the ONNX Model into the workload
-            UserDefinedModelParserStage,  # Parses the user-defined Model into the workload
-            HintLoopsPartitionedWorkloadGenerationStage,
-            ZigZagCoreMappingEstimationStage,
-            GeneticAlgorithmAllocationStage,
-        ],
-        accelerator=hardware,  # required by AcceleratorParserStage
-        workload_path=workload,  # required by ModelParserStage
-        mapping_path=mapping,  # required by ModelParserStage
-        loma_lpf_limit=6,  # required by LomaEngine
-        nb_ga_individuals=128,  # number of individuals in each genetic algorithm generation
-        nb_ga_generations=100,  # number of genetic algorithm generations
-        node_hw_performances_path=f"outputs/{node_hw_cost_pkl_name}.pickle",  # saves results to skip re-computation
-        plot_hof=True,  # Save schedule and memory usage plot of each individual in the Genetic Algorithm hall of fame
-        plot_file_name=True,
-        plot_full_schedule=True,
-        plot_data_transfer=True,
-        cn_define_mode=CN_define_mode,
-        hint_loops=hint_loops,
-        scheduler_candidate_selection="memory",
-    )
+    # Output paths
+    plot_file_name = f"-{experiment_id}-ga"
+    node_hw_performances_path = f"{output_path}/{experiment_id}-saved_cn_hw_cost.pickle"
+    scme_path = f"{output_path}/{experiment_id}-scme.pickle"
 
-    # Launch the MainStage
-    answers = mainstage.run()
-    return answers
+    if os.path.exists(scme_path):
+        scme = pickle_load(scme_path)
+    else:
+        mainstage = MainStage(
+            [  # Initializes the MainStage as entry point
+                AcceleratorParserStage,  # Parses the accelerator
+                StreamONNXModelParserStage,  # Parses the ONNX Model into the workload
+                LayerStacksGenerationStage,
+                HintLoopsGenerationStage,
+                HintLoopsPartitionedWorkloadGenerationStage,
+                ZigZagCoreMappingEstimationStage,
+                SetFixedAllocationPerformanceStage,
+                SchedulingOrderGenerationStage,
+                GeneticAlgorithmAllocationStage,
+            ],
+            accelerator=hardware,  # required by AcceleratorParserStage
+            workload_path=workload,  # required by ModelParserStage
+            mapping_path=mapping,  # required by ModelParserStage
+            loma_lpf_limit=6,  # required by LomaEngine
+            nb_ga_individuals=16,  # number of individuals in each genetic algorithm generation
+            nb_ga_generations=16,  # number of genetic algorithm generations
+            node_hw_performances_path=node_hw_performances_path,
+            plot_file_name=plot_file_name,
+            mode=mode,
+        )
+        # Launch the MainStage
+        answers = mainstage.run()
+        scme = answers[0]
+    return scme
 
 
 if __name__ == "__main__":
-    CN_define_mode = 1
-    hint_loops = [("OY", "all")]
+    from stream.visualization.memory_usage import plot_memory_usage
+    from stream.visualization.schedule import visualize_timeline_plotly
 
     accelerator = "stream/inputs/examples/hardware/tpu_like_quad_core.yaml"
     workload = "stream/inputs/examples/workload/resnet18.yaml"
@@ -57,37 +69,32 @@ if __name__ == "__main__":
 
     hw_name = "tpu_like_quad_core"
     wl_name = "resnet18"
-    experiment_id = f"{hw_name}-{wl_name}-CNmode_{CN_define_mode}-hintloop_{str(hint_loops)}"
-    node_hw_cost_pkl_name = f"saved_CN_HW_cost-{experiment_id}"
+    mode = "fused"
+    experiment_id = f"{hw_name}-{wl_name}"
+    output_path = "outputs"
 
-    scme, _ = get_hardware_performance_stream(
+    scme, _ = optimize_allocation_ga(
         accelerator,
         workload,
         mapping,
-        CN_define_mode,
-        hint_loops,
-        node_hw_cost_pkl_name,
+        mode,
+        experiment_id,
+        output_path,
     )
-
-    from stream.visualization.memory_usage import plot_memory_usage
-    from stream.visualization.schedule import plot_timeline_brokenaxes
 
     plot_full_schedule = True
     draw_dependencies = True
     plot_data_transfer = True
     section_start_percent = (0,)
     percent_shown = (100,)
-    timeline_fig_path = "outputs/schedule_plot.png"
-    memory_fig_path = "outputs/memory_plot.png"
-    energy_fig_path = "outputs/energy_plot.png"
-    plot_timeline_brokenaxes(
-        scme[0].workload,
-        scme[0].accelerator,
-        draw_dependencies,
-        section_start_percent,
-        percent_shown,
-        plot_data_transfer,
-        fig_path=timeline_fig_path,
+    schedule_fig_path = f"{output_path}/schedule_plot.png"
+    memory_fig_path = f"{output_path}/memory_plot.png"
+    energy_fig_path = f"{output_path}/energy_plot.png"
+    visualize_timeline_plotly(
+        scme=scme,
+        draw_dependencies=draw_dependencies,
+        draw_communication=True,
+        fig_path=schedule_fig_path,
     )
-    plot_memory_usage(scme[0].accelerator.memory_manager, fig_path=memory_fig_path)
+    plot_memory_usage(scme.accelerator.memory_manager, fig_path=memory_fig_path)
     # bar_plot_stream_cost_model_evaluations_breakdown([scme], fig_path=energy_fig_path)
