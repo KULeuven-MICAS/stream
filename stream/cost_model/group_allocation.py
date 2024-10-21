16354a3b2febcfd5cb14058ece753e249cf0f84e
RobinGeens <robin.geens@kuleuven.be>
2024-10-21 10:36:32 +0200

    allow inter and intra core tiling of same layerdim by changing groupIDManager

--- stream/cost_model/group_allocation.py
+++ stream/cost_model/group_allocation.py
@@ -1,7 +1,9 @@
 import logging
+import math
 from typing import TypeAlias
 
 from stream.workload.computation.computation_node import ComputationNode, LoopRanges
+from zigzag.datatypes import LayerDim
 
 logger = logging.getLogger(__name__)
 
@@ -20,11 +22,29 @@ class GroupIdManager:
         self.__id_count += 1
         return curr_id
 
-    def __extract_inter_core_ranges(self, tile_loop_ranges: LoopRanges):
+    def __get_range_identifier_single_dim(self, inter_core_layer_dim: LayerDim, current_range: tuple[int, int]):
+        """Given the layer dim for which the tile should be split inter core, and the range of the tile for this
+        layer dim, return a range identifier that determines the group ID for this tile range. The group ID for the
+        full tile is an aggregate of the tile identifiers of all tile ranges of layer dims that are split inter-core.
+
+        The total layer size of the given layer dim is split up into N equal parts, where N is the number of intra core
+        splits for the given layer dim (that will also be split inter core). The range identifier corresponds to the
+        given range modulo the size of the N equal parts.
+        """
+        nb_intra_core_splits = next(
+            (split for layer_dim, split in self.node.intra_core_tiling if layer_dim == inter_core_layer_dim), 1
+        )
+        range_size_per_intra_split = self.node.layer_dim_sizes[inter_core_layer_dim] // nb_intra_core_splits
+        range_adjusted_to_intra_split = tuple(i % range_size_per_intra_split for i in current_range)
+        return range_adjusted_to_intra_split
+
+    def __get_range_identifier(self, tile_loop_ranges: LoopRanges):
         """Given the loop ranges of a tile, return a hashable identifier that can be used to determine wether this
-        tile belongs on the same core as other tiles. Tiles with different ranges for the inter core tile dimensions
-        belong on different cores"""
-        return tuple([tile_loop_ranges[dim] for dim in self.inter_core_tiled_dims])
+        tile belongs on the same core as other tiles."""
+        return tuple(
+            self.__get_range_identifier_single_dim(layer_dim, tile_loop_ranges[layer_dim])
+            for layer_dim, _ in self.node.inter_core_tiling
+        )
 
     def get_group_id(self, tile_loop_ranges: LoopRanges) -> int:
         """Return the group id for the given loop ranges.
@@ -49,7 +69,7 @@ class GroupIdManager:
             return 0
 
         # Differentiate based on node's inter core tiling
-        range_identifier = self.__extract_inter_core_ranges(tile_loop_ranges)
+        range_identifier = self.__get_range_identifier(tile_loop_ranges)
 
         # This tile belongs together with previously seen tiles
         if range_identifier in self.groups:
