8dc2886d9dcfd2ba0b4e78a6a4da26f8e8a57931
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-01-10 13:56:02 +0100

    add other features

--- stream/compiler/transforms/convert_stream_to_aie.py
+++ stream/compiler/transforms/convert_stream_to_aie.py
@@ -1,10 +1,12 @@
+from dataclasses import dataclass, field
+
 from xdsl.context import MLContext
-from xdsl.dialects.builtin import IntegerAttr, MemRefType, ModuleOp, i32
+from xdsl.dialects.arith import ConstantOp
+from xdsl.dialects.builtin import IntegerAttr, MemRefType, ModuleOp, StringAttr, i32
 from xdsl.dialects.func import CallOp, FuncOp
-from xdsl.ir import Region
+from xdsl.ir import Operation, Region, SSAValue
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
-    GreedyRewritePatternApplier,
     PatternRewriter,
     PatternRewriteWalker,
     RewritePattern,
@@ -16,32 +18,72 @@ from xdsl_aie.dialects.aie import (
     Block,
     CoreOp,
     DeviceOp,
+    EndOp,
     ObjectFifoAcquireOp,
     ObjectFifoOp,
     ObjectFifoPortEnum,
     ObjectFIFOReleaseOp,
+    ObjectFIFOSubviewAccessOp,
     SymbolTable,
     TileOp,
+)
+from xdsl_aie.dialects.aiex import (
+    DmaMemcpyNdOp,
+    DmaWaitOp,
+    RuntimeSequenceOp,
 )
 
 from stream.compiler.dialects.stream import ComputationNodeOp, TransferOp
 
 
-def get_tile_op(value: str) -> TileOp:
+def get_tile(value: str) -> tuple[int, int]:
     if value == "Any":
-        return TileOp(IntegerAttr(0, i32), IntegerAttr(0, i32))
-    elif value == "Core(1)":
-        return TileOp(IntegerAttr(0, i32), IntegerAttr(2, i32))
+        return (0, 0)
+    elif value == "Core(0)":
+        return (0, 2)
     raise ValueError("unknown tile")
 
 
+@dataclass
+class TileOpManager:
+
+    device_op: DeviceOp
+
+    tile_ops: dict[tuple[int, int], TileOp] = field(init=False)
+
+    def __post_init__(self):
+
+        self.tile_ops = {}
+
+        # index existing tile ops
+        for op in self.device_op.region.walk():
+            if isinstance(op, TileOp):
+                self.tile_ops[(op.col.value.data, op.row.value.data)] = op
+
+    def insert_or_update(self, x: int, y: int) -> TileOp:
+
+        # return pre-existing op
+        if (x, y) in self.tile_ops:
+            return self.tile_ops[(x, y)]
+
+        # create and insert op
+        rewriter = Rewriter()
+        rewriter.insert_op(tile_op := TileOp(x, y), InsertPoint.at_start(self.device_op.region.block))
+        self.tile_ops[(x, y)] = tile_op
+        return tile_op
+
+
+@dataclass
 class TransferToObjectFIFOPattern(RewritePattern):
+
+    tile_op_manager: TileOpManager
     counter = 0
 
     @op_type_rewrite_pattern
     def match_and_rewrite(self, op: TransferOp, rewriter: PatternRewriter):
-        source_tile = get_tile_op(op.source.data)
-        dest_tile = get_tile_op(op.dest.data)
+
+        source_tile = self.tile_op_manager.insert_or_update(*get_tile(op.source.data))
+        dest_tile = self.tile_op_manager.insert_or_update(*get_tile(op.dest.data))
 
         of_name = f"of{self.counter}"
 
@@ -49,8 +91,8 @@ class TransferToObjectFIFOPattern(RewritePattern):
             elemNumber=IntegerAttr(1, i32),
             producerTile=source_tile,
             consumerTiles=[dest_tile],
-            referenced_type=op.results[0].type,
-            shape=[],
+            referenced_type=op.results[0].type.element_type,
+            shape=op.results[0].type.get_shape(),
             name=of_name,
         )
 
@@ -61,13 +103,12 @@ class TransferToObjectFIFOPattern(RewritePattern):
             device_op = device_op.parent
 
         SymbolTable.insert_or_update(device_op, object_fifo)
-        rewriter.insert_op([source_tile, dest_tile], InsertPoint.before(object_fifo))
 
         # decide whether to consume or produce
         if op.source.data == "Any":
-            port = ObjectFifoPortEnum.Produce
-        else:
             port = ObjectFifoPortEnum.Consume
+        else:
+            port = ObjectFifoPortEnum.Produce
 
         assert isinstance(memref_type := op.results[0].type, MemRefType)
 
@@ -79,6 +120,8 @@ class TransferToObjectFIFOPattern(RewritePattern):
             element_type=memref_type.get_element_type(),
         )
 
+        access_op = ObjectFIFOSubviewAccessOp(IntegerAttr(0, i32), acquire_op)
+
         release_op = ObjectFIFOReleaseOp(
             IntegerAttr.from_int_and_width(port.get_int(), 32),
             IntegerAttr.from_int_and_width(1, 32),
@@ -89,16 +132,28 @@ class TransferToObjectFIFOPattern(RewritePattern):
         last_use = list(op.results[0].uses)[-1].operation
         rewriter.insert_op(release_op, InsertPoint.after(last_use))
 
-        rewriter.replace_matched_op([acquire_op], new_results=acquire_op.results)
+        rewriter.replace_matched_op([acquire_op, access_op], new_results=access_op.results)
 
         # increment of counter
         self.counter += 1
 
 
+@dataclass
 class TestPatttern(RewritePattern):
+
+    tile_op_manager: TileOpManager
+
     @op_type_rewrite_pattern
     def match_and_rewrite(self, op: ComputationNodeOp, rewriter: PatternRewriter) -> None:
-        func_op = FuncOp(op.kernel.data, ([], []), Region(), "private")
+
+        input_types = [operand.type for operand in op.inputs]
+        if op.outputs:
+            input_types.append(op.outputs.type)
+
+        # four i32's?
+        input_types.extend([i32] * 4)
+
+        func_op = FuncOp(op.kernel.data, (input_types, []), Region(), "private")
 
         device_op = op
         while not isinstance(device_op, DeviceOp):
@@ -107,9 +162,54 @@ class TestPatttern(RewritePattern):
 
         SymbolTable.insert_or_update(device_op, func_op)
 
-        func_call = CallOp(op.kernel.data, [], [])
+        zero = ConstantOp.from_int_and_width(0, i32)
 
-        rewriter.replace_matched_op(func_call)
+        inputs: list[SSAValue | Operation] = list(op.inputs)
+        if op.outputs:
+            inputs.append(op.outputs)
+        inputs.extend([zero] * 4)
+
+        func_call = CallOp(op.kernel.data, inputs, [])
+
+        rewriter.replace_matched_op((zero, func_call))
+
+
+@dataclass
+class InsertRuntimeDMAs(RewritePattern):
+
+    sequence_op: RuntimeSequenceOp
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: ObjectFifoOp, rewriter: PatternRewriter):
+
+        # Add Block Argument to SequenceOp
+        memref_type = op.elemType.buffer
+        assert isinstance(memref_type, MemRefType)
+
+        sequence_block = self.sequence_op.body.block
+
+        sequence_block.insert_arg(memref_type, 0)
+
+        # Insert DMA
+        memcpy = DmaMemcpyNdOp(
+            0,
+            0,
+            sequence_block.args[0],
+            static_offsets=[0, 0, 0, 0],
+            static_sizes=[1, 1, 1, memref_type.get_shape()[0]],
+            static_strides=[0, 0, 0, 1],
+            metadata=op.sym_name,
+            id=0,
+            issue_token=True,
+        )
+
+        rewriter.insert_op(memcpy, InsertPoint.at_start(sequence_block))
+
+        # wait for it ...
+
+        wait = DmaWaitOp(op.sym_name)
+
+        rewriter.insert_op(wait, InsertPoint.at_end(sequence_block))
 
 
 class ConvertStreamToAIEPass(ModulePass):
@@ -120,7 +220,7 @@ class ConvertStreamToAIEPass(ModulePass):
 
         rewriter = Rewriter()
         device_op = DeviceOp(
-            IntegerAttr.from_int_and_width(AIEDeviceEnum.npu1.get_int(), 32),
+            IntegerAttr.from_int_and_width(AIEDeviceEnum.npu1_1col.get_int(), 32),
             rewriter.move_region_contents_to_new_regions(op.body),
         )
         op.body.add_block(Block([device_op]))
@@ -128,15 +228,29 @@ class ConvertStreamToAIEPass(ModulePass):
         # wrap everything in a core op
         core_tile = TileOp(0, 2)
 
-        core_op = CoreOp(None, core_tile, rewriter.move_region_contents_to_new_regions(device_op.region))
+        core_op = CoreOp(
+            None,
+            core_tile,
+            rewriter.move_region_contents_to_new_regions(device_op.region),
+            link_with=StringAttr("conv2dk1_i8.o"),
+        )
+        # add end op
+        rewriter.insert_op(EndOp(), InsertPoint.at_end(core_op.region.block))
         device_op.region.add_block(Block([core_tile, core_op]))
 
+        tile_op_manager = TileOpManager(device_op)
+
         PatternRewriteWalker(
-            GreedyRewritePatternApplier([TransferToObjectFIFOPattern()]),
+            TransferToObjectFIFOPattern(tile_op_manager),
+            apply_recursively=False,
+        ).rewrite_module(op)
+        PatternRewriteWalker(
+            TestPatttern(tile_op_manager),
             apply_recursively=False,
         ).rewrite_module(op)
 
-        PatternRewriteWalker(
-            GreedyRewritePatternApplier([TestPatttern()]),
-            apply_recursively=False,
-        ).rewrite_module(op)
+        # add a runtime sequence operation
+        runtime_sequence = RuntimeSequenceOp(Region(Block()))
+        rewriter.insert_op(runtime_sequence, InsertPoint.at_end(device_op.region.block))
+
+        PatternRewriteWalker(InsertRuntimeDMAs(runtime_sequence), apply_recursively=False).rewrite_module(op)
