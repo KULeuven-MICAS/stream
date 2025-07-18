d85a56bdb8aad7e685406fcfe07a33e58a848db6
Arne Symons <arne.symons@kuleuven.be>
2025-07-18 10:14:07 +0200

    initialize submodules in workflow

--- setup.sh
+++ setup.sh
@@ -11,10 +11,10 @@ python3 -m pip install mlir_aie -f https://github.com/Xilinx/mlir-aie/releases/e
 python3 -m pip install llvm-aie -f https://github.com/Xilinx/llvm-aie/releases/expanded_assets/nightly
 
 # Install basic Python requirements (still needed for release v1.0, but is no longer needed for latest wheels)
-# python3 -m pip install -r mlir-aie/python/requirements.txt
+python3 -m pip install -r mlir-aie/python/requirements.txt
 
 # Install MLIR Python Extras
-# HOST_MLIR_PYTHON_PACKAGE_PREFIX=aie python3 -m pip install -r mlir-aie/python/requirements_extras.txt
+HOST_MLIR_PYTHON_PACKAGE_PREFIX=aie python3 -m pip install -r mlir-aie/python/requirements_extras.txt
 
 # Source mlir-aie environment
 source mlir-aie/utils/env_setup.sh
