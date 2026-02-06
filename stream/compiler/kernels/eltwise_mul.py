e20a972f5df4f6100e4c3107ce997a3094abe100
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2026-02-06 10:11:32 +0100

    set kernel layouts for custom transformation

--- stream/compiler/kernels/eltwise_mul.py
+++ stream/compiler/kernels/eltwise_mul.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from math import prod
 from typing import cast
 
+from snaxc.ir.tsl import Stride, TiledStride, TiledStridedLayout
 from xdsl.dialects.arith import ConstantOp
 from xdsl.dialects.builtin import (
     AnyDenseElement,
@@ -28,6 +29,35 @@ class EltwiseMulKernel(AIEKernel):
     @property
     def function_name(self) -> str:
         return f"eltwise_mul_{self.element_type}_vector"
+
+    def operand_layouts(self) -> Sequence[TiledStridedLayout]:
+        # Intrinsic dimensions:
+        r = 4  # ~m
+        s = 8  # ~k
+        t = 8  # ~n
+        # Tiled kernel dimensions:
+        mt = 32 // r
+        nt = 64 // t
+        return [
+            TiledStridedLayout(  # default input
+                [
+                    TiledStride([Stride(r * t * nt, mt), Stride(nt * t, r)]),
+                    TiledStride([Stride(t, nt), Stride(1, t)]),
+                ]
+            ),
+            TiledStridedLayout(  # default input
+                [
+                    TiledStride([Stride(r * t * nt, mt), Stride(nt * t, r)]),
+                    TiledStride([Stride(t, nt), Stride(1, t)]),
+                ]
+            ),
+            TiledStridedLayout(  # tiled output
+                [
+                    TiledStride([Stride(r * t * nt, mt), Stride(t, r)]),
+                    TiledStride([Stride(r * t, nt), Stride(1, t)]),
+                ]
+            ),
+        ]
 
     def function_type(self, op: ComputationNodeOp) -> FunctionType:
         assert op.output is not None
