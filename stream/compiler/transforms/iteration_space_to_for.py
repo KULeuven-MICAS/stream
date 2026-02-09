4cd7fb8fb53a50f415d17c5b7ac5fad79478e9e7
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2026-02-09 10:28:52 -0700

    progress on getting larger swiglu working

--- stream/compiler/transforms/iteration_space_to_for.py
+++ stream/compiler/transforms/iteration_space_to_for.py
@@ -34,11 +34,19 @@ def iteration_space_to_for(block: Block, rewriter: Rewriter):  # noqa: PLR0912, 
     for_ops = []
     for_dict: dict[IterationVariable, ForOp] = {}
     vars: list[IterationVariable] = []
+    var_selector: list[bool] = []
     for var in ssis.variables:
         if var.type not in (IterationVariableType.TEMPORAL, IterationVariableType.SPATIOTEMPORAL):
+            if var.type != IterationVariableType.KERNEL:
+                var_selector.append(False)
             continue
         vars.append(var)
-        ub = ConstantOp.from_int_and_width(var.size, IndexType())
+        var_selector.append(True)
+        # If not relevant, set bound to 1 such that it is canonicalized away
+        if var.relevant:
+            ub = ConstantOp.from_int_and_width(var.size, IndexType())
+        else:
+            ub = ConstantOp.from_int_and_width(1, IndexType())
         for_op = ForOp(lb, ub, step, [], Block([*for_ops, YieldOp()], arg_types=[IndexType()]))
         for_op.attributes["layer_dim"] = StringAttr(str(var.dimension))
         for_ops = [ub, for_op]
@@ -48,14 +56,16 @@ def iteration_space_to_for(block: Block, rewriter: Rewriter):  # noqa: PLR0912, 
     rewriter.insert_op(for_ops, InsertPoint.after(step))
 
     for op in ops:
-        these_vars = [*op.ssis.data.get_spatio_temporal_variables(), *op.ssis.data.get_temporal_variables()]
-        # get rid of spatio temporal vars that are not present in compute
-        these_vars = these_vars[-len(vars) :]
-        if len(these_vars) != len(vars):
-            # FIXME: bring back ssis dest for push / pull ops
-            assert vars[0].type is IterationVariableType.SPATIOTEMPORAL
-            spatial_var = next(x for x in op.ssis.data.get_spatial_variables() if x.dimension == vars[0].dimension)
-            these_vars.insert(0, spatial_var)
+        non_kernel_vars = [var for var in op.ssis.data.variables if var.type != IterationVariableType.KERNEL]
+        these_vars = [var for var, selector in zip(non_kernel_vars, var_selector, strict=True) if selector]
+        # these_vars = [*op.ssis.data.get_spatio_temporal_variables(), *op.ssis.data.get_temporal_variables()]
+        # # get rid of spatio temporal vars that are not present in compute
+        # these_vars = these_vars[-len(vars) :]
+        # if len(these_vars) != len(vars):
+        #     # FIXME: bring back ssis dest for push / pull ops
+        #     assert vars[0].type is IterationVariableType.SPATIOTEMPORAL
+        #     spatial_var = next(x for x in op.ssis.data.get_spatial_variables() if x.dimension == vars[0].dimension)
+        #     these_vars.insert(0, spatial_var)
         # find first relevant one
         first_relevant = next((v for v in these_vars if v.relevant), None)
         if not first_relevant:
@@ -73,51 +83,3 @@ def iteration_space_to_for(block: Block, rewriter: Rewriter):  # noqa: PLR0912, 
         else:
             op.detach()
             rewriter.insert_op(op, after)
-
-    # # iterate over the steady state ops
-    # ops_iter = iter(ops)
-    # op = next(ops_iter, None)
-    # assert op is not None
-    # # get reversed list of iteration variables, from outermost to innermost
-    # ssis = op.ssis.data.get_temporal_variables()[::-1]
-    # ssis_op = op.ssis.data.get_temporal_variables()[::-1]
-    # for_op = None
-    # for i, iter_var in enumerate(ssis):
-    #     # if not any of the following ops are relevant
-    #     while op is not None and not any(iv.relevant for iv in ssis_op[i:]):
-    #         op.detach()
-    #         rewriter.insert_op(op, insertion_point)
-    #         insertion_point = InsertPoint.after(op)
-    #         op = next(ops_iter, None)
-    #         if op is not None:
-    #             ssis_op = op.ssis.data.get_temporal_variables()[::-1]
-    #     # create for loop
-    #     ub = ConstantOp.from_int_and_width(iter_var.size, IndexType())
-    #     for_op = ForOp(lb, ub, step, [], Block([yield_op := YieldOp()], arg_types=[IndexType()]))
-    #     for_op.attributes["layer_dim"] = StringAttr(str(iter_var.dimension))
-    #     rewriter.insert_op((ub, for_op), insertion_point)
-    #     insertion_point = InsertPoint.before(yield_op)
-    # assert for_op is not None
-    # # iterate in reverse now:
-    # # now, a non reversed version of the ssis:
-    # if op is None:
-    #     return
-    # ssis = op.ssis.data.get_temporal_variables()
-    # ssis_op = op.ssis.data.get_temporal_variables()
-    # for i, _ in enumerate(ssis):
-    #     while op is not None and ssis_op[i].relevant:
-    #         op.detach()
-    #         rewriter.insert_op(op, insertion_point)
-    #         insertion_point = InsertPoint.after(op)
-    #         op = next(ops_iter, None)
-    #         if op is not None:
-    #             ssis_op = op.ssis.data.get_temporal_variables()
-    #             if len(ssis_op) == 0:
-    #                 ssis_op = ssis
-    #     # move up
-    #     for_op = for_op.parent_op()
-    #     if not isinstance(for_op, ForOp):
-    #         assert i == len(ssis) - 1
-    #         break
-    #     assert isinstance(yield_op := for_op.regions[0].block.last_op, YieldOp)
-    #     insertion_point = InsertPoint.before(yield_op)
